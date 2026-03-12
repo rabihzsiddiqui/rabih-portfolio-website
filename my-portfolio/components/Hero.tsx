ed05761f8e63a4efd5bade6f8d670ccca5d6d738
Rabih Siddiqui <rabihzsiddiqui@gmail.com>
2026-03-11 22:38:40 -0700

    rename Get in Touch button to Email me

--- my-portfolio/components/Hero.tsx
+++ my-portfolio/components/Hero.tsx
@@ -116,7 +116,7 @@ export default function Hero() {
                 d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
               />
             </svg>
-            Get in Touch
+            Email me!
           </a>
         </div>
 
